fa37dadd5196aa03fb7c49961aeb09cb58206528
aharin <aharin@thoughtworks.com>
2011-01-21 17:55:37 +0000

    Make sure that request header are initialized to empty list

--- Libraries/Plasma.Core/AspNetRequest.cs
+++ Libraries/Plasma.Core/AspNetRequest.cs
@@ -8,29 +8,18 @@
  * You must not remove this notice, or any other, from this software.
  *
  * **********************************************************************************/
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Plasma.Core
 {
-
     public class AspNetRequest {
-        private string _filePath;
-        private string _pathInfo;
-        private string _queryString;
-        private string _method;
-        private List<KeyValuePair<string, string>> _headers;
-        private byte[] _body;
-
         public AspNetRequest(
-                string requestFilePath,
-                string requestPathInfo,
-                string requestQueryString,
-                string requestMethod,
-                List<KeyValuePair<string, string>> requestHeaders,
-                byte[] requestBody) {
-
+            string requestFilePath,
+            string requestPathInfo,
+            string requestQueryString,
+            string requestMethod,
+            List<KeyValuePair<string, string>> requestHeaders,
+            byte[] requestBody) {
             // BugBug: Start Hack
 
             string[] requestFilePathPieces = requestFilePath.Split('?');
@@ -42,51 +31,28 @@ namespace Plasma.Core
 
             // BugBug: End Hack
 
-            _filePath = requestFilePath;
-            _pathInfo = requestPathInfo;
-            _queryString = requestQueryString;
-            _method = requestMethod;
-            _headers = requestHeaders;
-            _body = requestBody;
+            FilePath = requestFilePath;
+            PathInfo = requestPathInfo;
+            QueryString = requestQueryString;
+            Method = requestMethod;
+            Headers = requestHeaders ?? new List<KeyValuePair<string, string>>();
+            Body = requestBody;
         }
 
-        public AspNetRequest(string requestPath) 
+        public AspNetRequest(string requestPath)
             : this(requestPath, null, null, "GET", null, null) {
         }
 
-        public string FilePath {
-            get { return _filePath; }
-            set { _filePath = value; }
-        }
+        public string FilePath { get; set; }
 
-        public string PathInfo {
-            get { return _pathInfo; }
-            set { _pathInfo = value; }
-        }
+        public string PathInfo { get; set; }
 
-        public string QueryString {
-            get { return _queryString; }
-            set { _queryString = value; }
-        }
+        public string QueryString { get; set; }
 
-        public string Method {
-            get { return _method; }
-            set { _method = value; }
-        }
+        public string Method { get; set; }
 
-        public List<KeyValuePair<string, string>> Headers {
-            get {
-                if (_headers == null) {
-                    _headers = new List<KeyValuePair<string, string>>();
-                }
+        public List<KeyValuePair<string, string>> Headers { get; private set; }
 
-                return _headers; 
-            }
-        }
-
-        public byte[] Body {
-            get { return _body; }
-            set { _body = value; }
-        }
+        public byte[] Body { get; set; }
     }
 }
