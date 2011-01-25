8c2b37d71fd33e7ce2b38ca220270d11a4768d29
aharin <aharin@thoughtworks.com>
2011-01-25 19:13:27 +0000

    Updated copyright

--- Libraries/Plasma.Core/AspNetResponse.cs
+++ Libraries/Plasma.Core/AspNetResponse.cs
@@ -2,6 +2,10 @@
  *
  * Copyright (c) Microsoft Corporation. All rights reserved.
  *
+ * Portions Copyright 2010 ThoughtWorks, Inc.
+ * ThoughtWorks provides the software "as is" without warranty of any kind, either express or implied, including but not limited to, 
+ * the implied warranties of merchantability, satisfactory quality, non-infringement and fitness for a particular purpose.
+ *
  * This source code is subject to terms and conditions of the Microsoft Permissive
  * License (MS-PL).
  *
