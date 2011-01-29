b25671dca860fa015c65876ef8c3477e21ee81c5
jennifersmith <jensmith@thoughtworks.com>
2011-01-29 22:31:27 +0000

    Added rake file and generated the assembly info

--- Libraries/Plasma.Samples.NUnitTestLibrary/Properties/AssemblyInfo.cs
+++ Libraries/Plasma.Samples.NUnitTestLibrary/Properties/AssemblyInfo.cs
@@ -1,35 +1,13 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Plasma.Samples.NUnitTestLibrary")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Plasma.Samples.NUnitTestLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyTitle("Plasma.Core")]
+[assembly: AssemblyDescription("Plasma provides web automation and testing capabilities to ASP.NET projects.")]
+[assembly: AssemblyCompany("n/a")]
+[assembly: AssemblyProduct("Plasma")]
+[assembly: AssemblyCopyright("Copyright 2011")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18bead12-ac4a-4d10-9fac-01af5ef1d665")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
+
