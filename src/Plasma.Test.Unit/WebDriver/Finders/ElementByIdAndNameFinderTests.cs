a7c38b0940dfdac071d9a6908d0faf3ac00448f9
jennifersmith@work <jennifersmith@work>
2011-04-26 16:08:48 +0100

    whoops missing file

---
+++ src/Plasma.Test.Unit/WebDriver/Finders/ElementByIdAndNameFinderTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using NUnit.Framework;
+using Plasma.WebDriver.Finders;
+
+namespace Plasma.Test.Unit.WebDriver.Finders
+{
+    [TestFixture]
+    public class ElementByIdAndNameFinderTests
+    {
+        [Test]
+        public void ShouldMatchIfThereIsAnElementWithTheGivenId()
+        {
+            const string xmlSource = @"<html>
+                                                    <body>
+                                                        <div>
+                                                            <div>
+                                                                <div id='elementToFind'>Find this</div>
+                                                            </div>
+                                                         </div>
+                                                    </body>N
+                                                </html>";
+
+            var document = new XmlDocument();
+            document.LoadXml(xmlSource);
+
+            IEnumerable<XmlElement> xmlElements = new ElementByIdFinder("elementToFind").FindWithin(document.DocumentElement);
+
+            Assert.That(xmlElements.Count(), Is.EqualTo(1));
+            Assert.That(xmlElements.Single().InnerText, Is.EqualTo("Find this"));
+        }
+
+        [Test]
+        public void ShouldNotMatchIfThereIsAnElementWithTheGivenId()
+        {
+
+            const string xmlSource = @"<html>
+                                                    <body>
+                                                        <div>
+                                                            <div>
+                                                                <div id='dontFindThis'>Don't find this</div>
+                                                            </div>
+                                                         </div>
+                                                    </body>N
+                                                </html>";
+
+            var document = new XmlDocument();
+            document.LoadXml(xmlSource);
+
+            IEnumerable<XmlElement> xmlElements = new ElementByIdFinder("elementToFind").FindWithin(document.DocumentElement);
+
+            Assert.That(xmlElements.Count(), Is.EqualTo(0));
+        }
+    }
+}
