70656b77b14cfdebdf4df4a3de804ec69b591a13
Jennifer Smith <jen@tw>
2011-04-28 11:35:30 +0100

    Arrght missing files

---
+++ src/Plasma.WebDriver/Finders/ElementByCssSelectorFinder.cs
@@ -0,0 +1,33 @@
+﻿/* **********************************************************************************
+ *
+ * Copyright 2010 ThoughtWorks, Inc.  
+ * ThoughtWorks provides the software "as is" without warranty of any kind, either express or implied, including but not limited to, 
+ * the implied warranties of merchantability, satisfactory quality, non-infringement and fitness for a particular purpose.
+ *
+ * This source code is subject to terms and conditions of the Microsoft Permissive
+ * License (MS-PL).  
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * **********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Plasma.WebDriver.Finders
+{
+    public class ElementByCssSelectorFinder : IElementFinder
+    {
+        private readonly string _cssSelector;
+
+        public ElementByCssSelectorFinder(string cssSelector)
+        {
+            _cssSelector = cssSelector;
+        }
+
+        public IEnumerable<XElement> FindWithin(XElement xmlElement)
+        {
+            throw new NotImplementedException("We have not yet implemented CSS selectors - coming soon");
+        }
+    }
+}
---
+++ src/Plasma.WebDriver/Finders/IElementFinder.cs
@@ -0,0 +1,22 @@
+﻿/* **********************************************************************************
+ *
+ * Copyright 2010 ThoughtWorks, Inc.  
+ * ThoughtWorks provides the software "as is" without warranty of any kind, either express or implied, including but not limited to, 
+ * the implied warranties of merchantability, satisfactory quality, non-infringement and fitness for a particular purpose.
+ *
+ * This source code is subject to terms and conditions of the Microsoft Permissive
+ * License (MS-PL).  
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * **********************************************************************************/
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Plasma.WebDriver.Finders
+{
+    public interface IElementFinder
+    {
+        IEnumerable<XElement> FindWithin(XElement xmlElement);
+    }
+}
