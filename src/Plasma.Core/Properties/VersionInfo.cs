0c34ff0f8812fd78b53c2f190f107fe772ea83dc
aharin <aharin@thoughtworks.com>
2011-05-23 10:00:40 +0100

    Updated versioninfo

--- src/Plasma.Core/Properties/VersionInfo.cs
+++ src/Plasma.Core/Properties/VersionInfo.cs
@@ -1,12 +1,4 @@
 using System.Reflection;
-<<<<<<< HEAD
-using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0.")]
-
-[assembly: AssemblyInformationalVersion("1.1.2.0 Alpha")]
-=======
->>>>>>> e2d3fd3b0039a48c9b17eff971cf59b4b7147fb2
 
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
