28b334a4ee98ea69ce1d6379da18a2c7f9331624
Tom Scott <tomwscott@gmail.com>
2011-11-07 10:10:41 +0000

    Adding batch file to run project so that you don't need to rely on msbuild being on the command line.

---
+++ run.bat
@@ -0,0 +1,19 @@
+@echo off
+SET PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319
+CALL MSBuild.exe /verbosity:n  %*
+IF ERRORLEVEL 1 goto RedBuild
+IF ERRORLEVEL 0 goto GreenBuild
+
+:RedBuild
+REM color 4f
+more tools\buildflags\failed.txt
+goto TheEnd
+
+:GreenBuild
+more tools\buildflags\passed.txt
+REM color 2F
+
+:TheEnd
+pause
+
+REM color 07
