828f8561cff5fb86ac6255c9d03d3d7b6e5aeee6
aharin <aharin@thoughtworks.com>
2013-11-12 23:08:50 +0000

    make sure that git is on the path

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-SET PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319
+SET PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319;%PROGRAMFILES(X86)%\Git\bin
 CALL MSBuild.exe Plasma.proj /verbosity:n  %*
 IF ERRORLEVEL 1 goto RedBuild
 IF ERRORLEVEL 0 goto GreenBuild
