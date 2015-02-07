03bc181b316f3e2ed01b91f1d7a9caee1cbe0eb6
David Whitney <david@davidwhitney.co.uk>
2015-02-07 14:43:36 +0000

    Upgraded build tools version to Visual Studio 2013 / Community Edition. Added nuspecs to generate packages in a way that they automatically reference their dependent generated packages.

---
+++ package.cmd
@@ -0,0 +1,3 @@
+.nuget\nuget pack "src\Plasma.Core\Plasma.Core.csproj" -OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive
+.nuget\nuget pack "src\Plasma.Http\Plasma.Http.csproj" -OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive
+.nuget\nuget pack "src\Plasma.WebDriver\Plasma.WebDriver.csproj" -OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive
--- src/Plasma.Http/Properties/AssemblyInfo.cs
+++ src/Plasma.Http/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Plasma.Http")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Plasma provides web automation and testing capabilities to ASP.NET projects.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Plasma.Http")]
